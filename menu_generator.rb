7b5067dc574ee3f63524d9bfb617710f3c01d460
Carly Jugler <carjug@gmail.com>
2015-05-16 21:23:43 -0700

    Removed unnecessary commented-out code, Added more descriptors

--- menu_generator.rb
+++ menu_generator.rb
@@ -1,18 +1,13 @@
 #
 texture = ["Gellatinous", "Spongy", "Semi-solid", "Pulpy", "Crystallized",
               "Frozen", "Condensed", "Liquid", "Loose", "Hot", "Fleshy",
-              "Plump", "Chunky", "Soupy"]
+              "Plump", "Chunky", "Soupy", "Putrid"]
 descriptor = ["Herbaceous", "Sludgy", "Savory", "Pungent", "Zesty",
               "Frothy", "Fermented", "Dull", "Stale", "Watery", "Boiled",
-              "Melted", "Sour"]
+              "Melted", "Sour", "Wad of"]
 food = ["Cereal", "Spam", "Beef Tartare", "Pig's Feet", "Tofu",
               "Mung Beans", "Cheese", "Hot Pocket", "Lean Cuisine",
               "Bologne"]
-
-#This is an attempt to compare and store all of the array's individual lengths
-#as a variable using conditionals
-#if (texture.length == descriptor.length) && (texture.length == food.length) \
-#    && (descriptor.length == food.length)
 
 menu = 1
 while menu <= texture.length
