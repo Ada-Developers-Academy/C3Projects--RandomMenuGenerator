4de24baa744b74be31a7d21138153974f7bb2c7a
Carly Jugler <carjug@gmail.com>
2015-05-16 21:36:25 -0700

    Added descriptors and welcome message

--- menu_generator.rb
+++ menu_generator.rb
@@ -1,16 +1,17 @@
-#
+puts "Welcome to the Kreepy Kavern! \nour menu today is:\n "
+
 texture = ["Gellatinous", "Spongy", "Semi-solid", "Pulpy", "Crystallized",
               "Frozen", "Condensed", "Liquid", "Loose", "Hot", "Fleshy",
               "Plump", "Chunky", "Soupy", "Putrid"]
 descriptor = ["Herbaceous", "Sludgy", "Savory", "Pungent", "Zesty",
               "Frothy", "Fermented", "Dull", "Stale", "Watery", "Boiled",
-              "Melted", "Sour", "Wad of"]
+              "Melted", "Sour", "Wad of", "Block of"]
 food = ["Cereal", "Spam", "Beef Tartare", "Pig's Feet", "Tofu",
               "Mung Beans", "Cheese", "Hot Pocket", "Lean Cuisine",
-              "Bologne"]
+              "Bologne", "Mayonnaise", "Saurkraut", ]
 
 menu = 1
 while menu <= texture.length
   puts menu.to_s + ". " + texture.sample + ' ' + descriptor.sample + ' ' + food.sample
-  menu = menu + 1
+  menu += 1
 end
