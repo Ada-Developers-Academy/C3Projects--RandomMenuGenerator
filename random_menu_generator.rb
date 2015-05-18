3298c5247b4c8b03debd91b833775047876a1873
amirahaile <amira.dhaile@gmail.com>
2015-05-17 20:11:40 -0700

    Lunch

--- random_menu_generator.rb
+++ random_menu_generator.rb
@@ -1,13 +1,14 @@
-adjectives = ["Creamy", "Tantilizing", "Toe-curling", "Finger-lickin'", "Savory", "Sweet", "Mushy", "Moist", "Spicy", "Rock-hard"]
-cooking_method = ["Toasted", "Seared", "Glazed", "Handmade", "Baked", "Smoked", "Grilled", "Sautéed", "Fried", "Squashed"]
-food = ["Reindeer", "Strawberry Cake", "Rack of Lamb", "Veggies", "Plantains", "Hot Toddies", "Three-bean Salad", "Shrimp", "Sushi"]
+adjectives = ["Creamy", "Tantilizing", "Toe-curling", "Finger-lickin'", "Savory", "Sweet", "Mushy", "Moist", "Spicy", "Rock-hard", "Boozy"]
+cooking_method = ["Toasted", "Seared", "Glazed", "Handmade", "Baked", "Smoked", "Grilled", "Sautéed", "Fried", "Squashed", "Flambéed"]
+food = ["Reindeer", "Strawberry Cake", "Rack of Lamb", "Veggies", "Plantains", "Hot Toddies", "Three-bean Salad", "Shrimp", "Sushi", "Horse Burger"]
 
 loop = 1
+longest_array = [adjectives.length, cooking_method.length, food.length].max
 
-10.times do
-  i = Random.rand(9)
-  j = Random.rand(9)
-  k = Random.rand(9)
-  puts "#{loop}. #{adjectives[i]} #{cooking_method[j]} #{food[k]}"
+longest_array.times do
+  a = Random.rand(adjectives.length - 1)
+  cm = Random.rand(cooking_method.length - 1)
+  f = Random.rand(food.length - 1)
+  puts "#{loop}. #{adjectives[a]} #{cooking_method[cm]} #{food[f]}"
   loop += 1
 end
