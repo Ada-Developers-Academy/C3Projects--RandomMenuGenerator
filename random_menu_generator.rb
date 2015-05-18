1bfa0db86ba14e0fd7b50e0608ebeb031e83dec9
amirahaile <amira.dhaile@gmail.com>
2015-05-17 21:04:00 -0700

    Dinner (2/2): User decides how many menu items to show.

--- random_menu_generator.rb
+++ random_menu_generator.rb
@@ -2,9 +2,17 @@ adjectives = ["Creamy", "Tantilizing", "Toe-curling", "Finger-lickin'", "Savory"
 cooking_method = ["Toasted", "Seared", "Glazed", "Handmade", "Baked", "Smoked", "Grilled", "Sautéed", "Fried", "Squashed", "Flambéed"]
 food = ["Reindeer", "Strawberry Cake", "Rack of Lamb", "Veggies", "Plantains", "Hot Toddies", "Three-bean Salad", "Shrimp", "Sushi", "Horse Burger"]
 
+puts "How many menu items would you like to see the Random Menu Generator generate?"
+num_of_items = gets.chomp.to_i
+
 # finds the length of the shortest array
 # ensures generated menu has all 3 categories
 min_items = [adjectives.length, cooking_method.length, food.length].min
+
+if num_of_items > min_items
+  puts "\nI'm sorry, we're only prepared to generate #{min_items} menu items.\n"
+  num_of_items = min_items
+end
 
 # randomizes each categories' items into a new array
 rando_adjective = adjectives.sample(min_items)
@@ -13,7 +21,7 @@ rando_food = food.sample(min_items)
 
 count = 1
 # ASSEMBLES THE LIST
-min_items.times do
+num_of_items.times do
   # don't forget to count by indices (hence [count - 1])
   puts "#{count}. #{rando_adjective[count - 1]} #{rando_cooking_method[count - 1]} #{rando_food[count - 1]}"
   count += 1
