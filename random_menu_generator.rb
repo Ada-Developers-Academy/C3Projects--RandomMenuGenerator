32e6c1ea6dd230a5772043e8acaf246f8d0eaa87
amirahaile <amira.dhaile@gmail.com>
2015-05-17 21:00:07 -0700

    Dinner (1/2): No items are ever repeated.

--- random_menu_generator.rb
+++ random_menu_generator.rb
@@ -2,13 +2,19 @@ adjectives = ["Creamy", "Tantilizing", "Toe-curling", "Finger-lickin'", "Savory"
 cooking_method = ["Toasted", "Seared", "Glazed", "Handmade", "Baked", "Smoked", "Grilled", "Sautéed", "Fried", "Squashed", "Flambéed"]
 food = ["Reindeer", "Strawberry Cake", "Rack of Lamb", "Veggies", "Plantains", "Hot Toddies", "Three-bean Salad", "Shrimp", "Sushi", "Horse Burger"]
 
-loop = 1
-longest_array = [adjectives.length, cooking_method.length, food.length].max
+# finds the length of the shortest array
+# ensures generated menu has all 3 categories
+min_items = [adjectives.length, cooking_method.length, food.length].min
 
-longest_array.times do
-  a = Random.rand(adjectives.length - 1)
-  cm = Random.rand(cooking_method.length - 1)
-  f = Random.rand(food.length - 1)
-  puts "#{loop}. #{adjectives[a]} #{cooking_method[cm]} #{food[f]}"
-  loop += 1
+# randomizes each categories' items into a new array
+rando_adjective = adjectives.sample(min_items)
+rando_cooking_method = cooking_method.sample(min_items)
+rando_food = food.sample(min_items)
+
+count = 1
+# ASSEMBLES THE LIST
+min_items.times do
+  # don't forget to count by indices (hence [count - 1])
+  puts "#{count}. #{rando_adjective[count - 1]} #{rando_cooking_method[count - 1]} #{rando_food[count - 1]}"
+  count += 1
 end
